9426428becd6ad1dc67bccefaccf46729ccce60f
Vaibhav Shedole <vaibhavshedole614@gmail.com>
2026-02-02 22:57:10 +0530

    Add simple calculator Java program

---
+++ Simplecalc.java
@@ -0,0 +1,39 @@
+// simple calc using java
+import java.util.Scanner;
+
+public class Simplecalc {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the a : ");
+        int a = sc.nextInt(); 
+        System.out.println("Enter operator : ");
+        char operator = sc.next().charAt(0);  
+        System.out.println("Enter b : "); 
+        int b = sc.nextInt();
+       
+
+        switch (operator) {
+            case '+': {System.out.println(a+b);
+                break;}
+            case '-':  {System.out.println(a-b);
+                break;
+            }  
+            case '*': {System.out.println(a*b);
+                break;
+            }
+            case '/': {System.out.println(a/b);
+                break;
+            }  
+            case '%': { System.out.println(a%b);
+                break;}          
+         default:
+            System.out.println("wrong operators");
+              
+        }
+        
+      
+        }
+    }
+    
+
+
